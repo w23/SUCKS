e94b46d694bb01603fdb41509b5124d6be688201
Ivan Avdeev <me@w23.ru>
2020-04-26 02:11:23 -0700

    step towards transfer loop

--- src/socks.rs
+++ src/socks.rs
@@ -14,7 +14,7 @@ use {
 };
 use byteorder::{NetworkEndian, ReadBytesExt};
 
-use crate::ringbuf;
+use crate::ringbuf::RingByteBuffer;
 use crate::ste;
 
 #[derive(Debug)]
@@ -131,193 +131,81 @@ fn readRequest(buf: &[u8]) -> std::io::Result<Request> {
     return Ok(Request{cmd, addr, port});
 }
 
-// impl Connection {
-//     fn handleClientRequest(&mut self, ctx: &ConnectionContext) -> Result<(), std::io::Error> {
-//         let request = readRequest(&mut self.client_stream)?;
-//         info!("C{}: readRequest => {:?}", self.seq, request);
-//
-//         match request.cmd {
-//             Command::Connect => {},
-//             _ => return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Command {:?} not implemented", request.cmd)))
-//         }
-//
-//         let remote_addr = request.socket_addr();
-//         self.test_remote_stream = Some(TcpStream::connect(&remote_addr)?);
-//         self.test_handle_remote_stream = Some(Connection::handleRemoteData);
-//         ctx.register(self.seq, 1, self.test_remote_stream.as_ref().unwrap())?;
-//
-//         // Reply to client
-//         // FIXME WouldBlock
-//         if let Err(e) = self.client_stream.write(&[0x05u8,0x00,0x00,0x01,0,0,0,0,0,0]) {
-//            return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("socket write error: {:?}", e)))
-//         }
-//
-//         self.handle_client_stream = Connection::handleClientData;
-//         Ok(())
-//     }
-//
-//     fn handleClientData(&mut self, _ctx: &ConnectionContext) -> Result<(), std::io::Error> {
-//         // FIXME push data in only available direction
-//         transerfignRealnosti(self.test_remote_stream.as_mut().unwrap(), &mut self.client_stream, &mut self.test_from_remote)?;
-//         transerfignRealnosti(&mut self.client_stream, self.test_remote_stream.as_mut().unwrap(), &mut self.test_to_remote)
-//     }
-//
-//     fn handleRemoteData(&mut self, _ctx: &ConnectionContext) -> Result<(), std::io::Error> {
-//         transerfignRealnosti(&mut self.client_stream, self.test_remote_stream.as_mut().unwrap(), &mut self.test_to_remote)?;
-//         transerfignRealnosti(self.test_remote_stream.as_mut().unwrap(), &mut self.client_stream, &mut self.test_from_remote)
-//     }
-//
-//     fn deregister(&self, poll: &Poll) -> Result<(), std::io::Error> {
-//         trace!("C{}: deregister", self.seq);
-//         poll.deregister(&self.client_stream)?;
-//         if let Some(ref stream) = self.test_remote_stream {
-//             poll.deregister(stream)?;
-//         }
-//
-//         Ok(())
-//     }
-// }
-//
-// enum ConnectionState {
-//     Handshake,
-//     Request,
-//     Connect,
-//     Transfer
-// }
-//
-// struct Connection {
-//     client_socket: Option<ste::StreamSocket>,
-//     state: ConnectionState,
-// }
-//
-// impl Connection {
-//     fn new() -> Connection {
-//         Connection {
-//             client_socket: None,
-//             state: ConnectionState::Handshake,
-//         }
-//     }
-//
-//     fn client_handler(this: Rc<RefCell<Connection>>) -> ste::StreamHandler {
-//         let clone = this.clone();
-//         let created = Box::new(move |socket| {
-//             clone.borrow_mut().created(socket)
-//         });
-//         let clone = this.clone();
-//         let push = Box::new(move |buf: &[u8]| {
-//             clone.borrow_mut().push(buf)
-//         });
-//         ste::StreamHandler {
-//             created,
-//             push,
-//             pull: Box::new(move |buf| {
-//                 this.borrow_mut().pull(buf)
-//             }),
-//             error: Box::new(move |err| {
-//                 error!("Connection error: {:?}", err);
-//             }),
-//         }
-//     }
-//
-//     fn created(&mut self, socket: ste::StreamSocket) {
-//         self.client_socket = Some(socket);
-//     }
-//
-//     fn push(&mut self, buf: &[u8]) -> usize {
-//         debug!("push {}", buf.len());
-//
-//         match self.state {
-//             ConnectionState::Handshake => {
-//                 // TODO: handle buffer wraparound
-//                 let connect = match readConnect(buf) {
-//                     None => return buf.len(),
-//                     Some(connect) => {
-//                         connect
-//                     },
-//                 };
-//
-//                 trace!("Received connect: {:?}", connect);
-//                 // FIXME write(..).unwrap? seriously?
-//                 trace!("Written: {}", self.client_socket.as_mut().unwrap().write(&[0x05u8,0x00]).unwrap());
-//
-//                 self.state = ConnectionState::Request;
-//                 return buf.len();
-//             },
-//             ConnectionState::Request => {
-//                 // TODO: handle buffer wraparound
-//                 let request = match readRequest(buf) {
-//                     Ok(request) => {
-//                         request
-//                     },
-//                     Err(err) if err.kind() == std::io::ErrorKind::UnexpectedEof => {
-//                         // FIXME: code dedup
-//                         return buf.len();
-//                     },
-//                     Err(err) => {
-//                         error!("Error reading request: {:?}", err);
-//                         return buf.len();
-//                     },
-//                 };
-//
-//                 info!("Read request: {:?}", request);
-//
-//                 // FIXME create socket to remote machine
-//
-//                 // FIXME write(..).unwrap? seriously?
-//                 trace!("Written: {}", self.client_socket.as_mut().unwrap().write(&[0x05u8,0x00,0x00,0x01,0,0,0,0,0,0]).unwrap());
-//
-//                 self.state = ConnectionState::Connect;
-//                 return buf.len();
-//             },
-//             ConnectionState::Connect => {
-//                 unimplemented!("");
-//             },
-//             ConnectionState::Transfer => {
-//                 unimplemented!("");
-//             }
-//         }
-//     }
-//
-//     fn pull(&mut self, buf: &mut [u8])-> usize {
-//         debug!("pull {}", buf.len());
-//         0
-//     }
-// }
+struct ReadWritePipe<T: Read+Write> {
+    pipe: T,
+    drained: bool,
+    full: bool,
+}
 
+impl<T: Read+Write> ReadWritePipe<T> {
+    fn new(pipe: T) -> ReadWritePipe<T> {
+        ReadWritePipe{pipe, drained: true, full: true }
+    }
+
+    fn read(&mut self, buffer: &mut RingByteBuffer) -> std::io::Result<usize> {
+        let mut total = 0;
+        loop {
+            if self.drained { break; }
+            let read = {
+                let buf = buffer.get_free_slot();
+                if buf.len() == 0 { break; }
+                let read = match self.pipe.read(buf) {
+                    Ok(size) => size,
+                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => { break; },
+                    Err(e) => return Err(e),
+                };
+                self.drained = read < buf.len();
+                read
+            };
+            total += read;
+            buffer.produce(read);
+        }
+        Ok(total)
+    }
+
+    fn write(&mut self, buffer: &mut RingByteBuffer) -> std::io::Result<usize> {
+        let mut total = 0;
+        loop {
+            if self.full { break; }
+            let written = {
+                let buf = buffer.get_data();
+                if buf.len() == 0 { break; }
+                let written = match self.pipe.write(buf) {
+                    Ok(size) => size,
+                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => { break; },
+                    Err(e) => return Err(e),
+                };
+                self.full = written < buf.len();
+                written
+            };
+            total += written;
+            buffer.consume(written);
+        }
+        Ok(total)
+    }
+}
+
+#[derive(PartialEq)]
 enum ConnectionState {
     Handshake,
     Request,
     Transfer,
-    TransferTail,
 }
 
 struct Connection {
     handle: Option<ste::Handle>,
     state: ConnectionState,
-    client: TcpStream,
-    remote: Option<TcpStream>,
-    client_read_buf: ringbuf::RingByteBuffer,
-    client_write_buf: ringbuf::RingByteBuffer,
-}
-
-impl Connection {
-    fn new(client: TcpStream) -> Connection {
-        Connection {
-            handle: None,
-            state: ConnectionState::Handshake,
-            client,
-            remote: None,
-            client_read_buf: ringbuf::RingByteBuffer::new(),
-            client_write_buf: ringbuf::RingByteBuffer::new(),
-        }
-    }
+    client: ReadWritePipe<TcpStream>,
+    remote: Option<ReadWritePipe<TcpStream>>,
+    client_read_buf: RingByteBuffer,
+    client_write_buf: RingByteBuffer,
 }
 
 impl ste::Context for Connection {
     fn registered(&mut self, ste: &mut ste::Ste, handle: ste::Handle) {
         info!("Handle: {:?}", handle);
         self.handle = Some(handle);
-        ste.register_source(handle, &mut self.client, 0);
+        ste.register_source(handle, &mut self.client.pipe, 0);
     }
 
     fn event(&mut self, ste: &mut ste::Ste, token: usize, event: &mio::event::Event) {
@@ -330,52 +218,42 @@ impl ste::Context for Connection {
 }
 
 impl Connection {
+    fn new(client: TcpStream) -> Connection {
+        Connection {
+            handle: None,
+            state: ConnectionState::Handshake,
+            client: ReadWritePipe::new(client),
+            remote: None,
+            client_read_buf: RingByteBuffer::new(),
+            client_write_buf: RingByteBuffer::new(),
+        }
+    }
+
     fn handleServer(&mut self, event: &mio::event::Event) {
-        unimplemented!();
+        assert!(self.state == ConnectionState::Transfer);
+        let remote = self.remote.as_mut().unwrap();
+        if event.is_readable() { remote.drained = false; }
+        if event.is_writable() { remote.full = false; }
+
+        loop {
+            remote.read(&mut self.client_write_buf).unwrap(); // FIXME handle
+            self.client.write(&mut self.client_write_buf).unwrap(); // FIXME handle
+
+            self.client.read(&mut self.client_read_buf).unwrap(); // FIXME handle
+            remote.write(&mut self.client_write_buf).unwrap(); // FIXME handle
+        }
     }
 
     fn handleClient(&mut self, ste: &mut ste::Ste, event: &mio::event::Event) {
         if event.is_readable() {
-            loop {
-                let (read, drained) = {
-                    let buf = self.client_read_buf.get_free_slot();
-                    if buf.len() == 0 { break; }
-                    let read = match self.client.read(buf) {
-                        Ok(size) => size,
-                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => { break; },
-                        Err(e) => {
-                            error!("Cannot read from client socket: {:?}", e);
-                            // FIXME what to do now?!
-                            return;
-                        }
-                    };
-                    (read, read < buf.len())
-                };
-                self.client_read_buf.produce(read);
-                if drained { break; }
-            }
+            self.client.drained = false;
+        }
+        if event.is_writable() {
+            self.client.full = false;
         }
 
-        if event.is_writable() {
-            loop {
-                let (written, full) = {
-                    let buf = self.client_write_buf.get_data();
-                    if buf.len() == 0 { break; }
-                    let written = match self.client.write(buf) {
-                        Ok(size) => size,
-                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => { break; },
-                        Err(e) => {
-                            error!("Cannot write to client socket: {:?}", e);
-                            // FIXME what to do now?!
-                            return;
-                        }
-                    };
-                    (written, written < buf.len())
-                };
-                self.client_read_buf.consume(written);
-                if full { break; }
-            }
-        }
+        self.client.read(&mut self.client_read_buf);
+        self.client.write(&mut self.client_write_buf);
 
         match self.state {
             ConnectionState::Handshake => {
@@ -392,7 +270,7 @@ impl Connection {
 
                 trace!("Received connect: {:?}", connect);
                 // FIXME write(..).unwrap? seriously?
-                trace!("Written: {}", self.client.write(&[0x05u8,0x00]).unwrap());
+                trace!("Written: {}", self.client.pipe.write(&[0x05u8,0x00]).unwrap());
 
                 self.state = ConnectionState::Request;
             },
@@ -421,14 +299,14 @@ impl Connection {
                 // FIXME create socket to remote machine
                 //ste.connect_stream(/*&request.addr*/ "localhost", self.handle.unwrap());
                 self.remote = Some(match TcpStream::connect(request.socket_addr()) {
-                    Ok(socket) => socket,
+                    Ok(socket) => ReadWritePipe::new(socket),
                     Err(e) => {
                         error!("Cannot connect to remote {:?}: {:?}", request, e);
                         unimplemented!();
                     }
                 });
 
-                match ste.register_source(self.handle.unwrap(), self.remote.as_mut().unwrap(), 1) {
+                match ste.register_source(self.handle.unwrap(), &mut self.remote.as_mut().unwrap().pipe, 1) {
                     Err(e) => {
                         error!("Cannot register remote socket {:?}: {:?}", request, e);
                         unimplemented!();
@@ -437,12 +315,13 @@ impl Connection {
                 }
 
                 // FIXME write(..).unwrap? seriously?
-                trace!("Written: {}", self.client.write(&[0x05u8,0x00,0x00,0x01,0,0,0,0,0,0]).unwrap());
+                trace!("Written: {}", self.client.pipe.write(&[0x05u8,0x00,0x00,0x01,0,0,0,0,0,0]).unwrap());
 
                 self.state = ConnectionState::Transfer;
             },
             ConnectionState::Transfer => {
-                unimplemented!("");
+                loop {
+                }
             }
         }
 
